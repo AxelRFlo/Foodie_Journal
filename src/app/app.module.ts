1b7ff2fbdf5f6bcdc0a5c8f7e08952e30f779a61
Marielle Cespedes <m.cespedes@accenture.com>
2018-02-01 17:31:20 -0600

    added food types (Wip)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { FoodTypesComponent } from './food-types/food-types.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FoodTypesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/food-types/food-types.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-food-types',
+  templateUrl: './food-types.component.html',
+  styleUrls: ['./food-types.component.scss']
+})
+export class FoodTypesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
