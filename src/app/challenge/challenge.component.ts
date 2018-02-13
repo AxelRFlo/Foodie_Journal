197cb30011dd088e60a6c3069e5bac3159b8def4
Ramirez <axel.ramirez@accenture.com>
2018-02-13 12:53:48 -0600

    HAHAHAHAHA

--- src/app/challenge/challenge.component.ts
+++ src/app/challenge/challenge.component.ts
@@ -51,16 +51,10 @@ export class ChallengeComponent implements OnInit {
       console.log(this.idRest);
 
       this.InfoRest = this._YelpService.GetYelpRestaurant(this.idRest);
-      //
-      if(!this._YelpService.ValidChallengeURL(this.InfoRest.categories,this.path,this.challenge)){
-        this._router.navigate(['/home']);
-      }
 
       console.log(this.InfoRest);
-      console.log(this.InfoRest.hours[0]["open"]);
       // Especificamos cual es mi journey actual en LS
       });
-      
   }
 
   jour(): void {
@@ -113,7 +107,6 @@ completeChallenge() {
         // We mark this challenge as completed
         this._YelpService.LSSet(savePath, 'completed');
       }
-  
     }
 
 
@@ -125,70 +118,11 @@ completeChallenge() {
   console.log('Challenge state: ' + this.challengeState);
   this.challengeState = this._YelpService.LSGet(this.lsChallengeState);
 
-
-
-
 }
 
 
 
 
 
-  completeChallenge2() {
-    // Si nunca le he dado click
 
-    // Checo si este challenge ya se grabó
-    // si no, pongo que ya se hizo etc.
-    // Si existe la variable, saco el valor, lo paso a number 
-    console.log('Tipo de comida...: ' + this.path);
-    console.log('Id del challenge es... ' + this.challenge);
-    console.log('CHALLENGE STATE EN LOCAL STORAGE: ' + this._YelpService.LSGet(this.lsChallengeState));
-
-    // Si tengo algo guardado 1, 2
-    if(this._YelpService.LSGet(this.lsChallengeState)) {
-      if (this._YelpService.LSGet(this.lsChallengeState) == 0){
-        // Si es cero lo cambiamos a 1
-        this.challengeState = 1;
-        this._YelpService.LSSet(this.lsChallengeState, this.challengeState);
-        console.log('Guarde esto en localstorage' + this.challengeState);
-      } else { 
-        // Si es 1 lo cambiamos a 2
-        if (this._YelpService.LSGet(this.lsChallengeState) == 1)){
-          this.challengeState = 2;
-          this._YelpService.LSSet(this.lsChallengeState, 2);
-        }
-      }
-      console.log('Mi save path: ' + this.savePath);
-     if (this._YelpService.LSGet(this.savePath)) {
-      console.log('Este challenge ya se ha completado.');
-
-    } else {
-       console.log('Este challenge no se ha completado aun.');
-      // Verifico si tengo progreso en este foodtype
-    if (this._YelpService.LSGet(this.path)) {
-      let completion = this._YelpService.LSGet(this.path);
-       console.log('completion de este foodtype: ' + completion);
-       let completion2 = parseInt(completion);
-       console.log('Completion parseada: ' + completion2);
-      completion2 = completion2 + 1;
-       console.log('Resultado de la suma' + completion2);
-      // We update the foodtype completed challenges
-      this._YelpService.LSSet(this.path, completion2);
-      // We mark this challenge as completed
-      this._YelpService.LSSet(this.savePath, 'completed');
-    } else {
-      // Si no existe progreso en este challenge
-      this._YelpService.LSSet(this.path, 1);
-      console.log('Wrote this' + this._YelpService.LSGet(this.path));
-      // We mark this challenge as completed
-      this._YelpService.LSSet(savePath, 'completed');
-    }
-
-  }
-
-     console.log("Finished function.");
-     console.log('Mi estado del challenge en localstorage es: ' + this._YelpService.LSGet(lsChallengeState))
-
-    // Le agrego uno, y lo guardo en this.path en local storage
-  }
 }
