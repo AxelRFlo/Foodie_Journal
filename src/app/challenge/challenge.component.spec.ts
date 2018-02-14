2bc1274e9e772f94f811083166b3861314ea7e48
Lilia Cabrieles <l.cabrieles.elizondo@accenture.com>
2018-02-14 16:08:10 -0600

    mm

--- src/app/challenge/challenge.component.spec.ts
+++ src/app/challenge/challenge.component.spec.ts
@@ -1,14 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChallengeComponent } from './challenge.component';
+import { RouterModule } from '@angular/router/src/router_module';
+import { ActivatedRoute } from '@angular/router/src/router_state';
 
 describe('ChallengeComponent', () => {
   let component: ChallengeComponent;
   let fixture: ComponentFixture<ChallengeComponent>;
+  let routers: RouterModule;
+  // let activatedroutes: ActivatedRoute;
 
   beforeEach(async(() => {
+    routers = {};
     TestBed.configureTestingModule({
-      declarations: [ ChallengeComponent ]
+      declarations: [ ChallengeComponent ],
+      imports: [RouterModule ],
+      providers: [
+        {provide: RouterModule, useValue: routers}
+      ],
     })
     .compileComponents();
   }));
